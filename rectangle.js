8a3c3e4fd86cc419801582fa2f4efab066476c03
GFko <gacho.patrik@gmail.com>
2023-01-29 20:28:54 +0100

    Initial code

---
+++ rectangle.js
@@ -0,0 +1,10 @@
+const rectangle = {
+    "length": 8,
+    "width": 10,
+  };
+
+  function calculateArea(length, width) {
+    console.log("Plocha obdĺžnika: " + length * width + "m²");
+};
+
+calculateArea(rectangle.length, rectangle.width);
